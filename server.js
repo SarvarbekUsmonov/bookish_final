edb037722d5f845ed5ef8a0f15069f319dc7dca2
sarvarbekusmonov <123qazwers321@gmail.com>
2023-05-04 02:45:53 -0700

    wq

--- server.js
+++ server.js
@@ -26,10 +26,13 @@ const port = 4000;
 app.use("/*.html", authenticate);
 app.use(express.static('public_html'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://67.205.167.183:3000'
+  }));
 
 
 app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://67.205.167.183:3000");
     res.setHeader("Access-Control-Allow-Origin", "http://67.205.167.183:3000");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
